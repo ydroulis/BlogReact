c2fca3643e6bab2886e869f433d2ad10d38c2433
ydroulis <lombardi.droulis@gmail.com>
2023-10-14 13:45:41 -0300

    fix import path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global-styles.css'
-import { Home } from './Templates/Home';
+import { Home } from './templates/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

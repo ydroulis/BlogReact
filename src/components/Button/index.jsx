c2f4cf7455fa2b9c712f1d0cdeb76670c824099f
ydroulis <lombardi.droulis@gmail.com>
2023-10-14 14:14:10 -0300

    transform calss component into a function

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,14 @@
-import { Component } from "react";
 import './styles.css';
 
-export class Button extends Component {
-  render() {
-    const { text, onClick, disabled } = this.props;
-    return <button className="button" onClick={onClick} disabled={disabled}>{text}</button>;
-  }
+export const Button = ({ text, onClick, disabled }) => {
+  return <button className="button" onClick={onClick} disabled={disabled}>{text}</button>
 }
+
+//CLASS COMPONENT
+
+// export class Button extends Component {
+//   render() {
+//     const { text, onClick, disabled } = this.props;
+//     return <button className="button" onClick={onClick} disabled={disabled}>{text}</button>;
+//   }
+// }

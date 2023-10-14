a4fa7bd25bf28a80632051872eaea48c7dc43377
ydroulis <lombardi.droulis@gmail.com>
2023-10-14 13:59:57 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/global-styles.css'
+import './styles/global-styles.css';
 import { Home } from './templates/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

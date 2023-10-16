fd3f095f9cb481cac1ae4b1ccc5c27965bcae390
ydroulis <lombardi.droulis@gmail.com>
2023-10-16 15:58:50 -0300

    create text input test

---
+++ src/components/TextInput/TextInput.spec.jsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react"
+import { TextInput } from "."
+import userEvent from "@testing-library/user-event";
+
+describe("<TextInput/>", () => {
+    const fn = jest.fn();
+    it("should have a value of searchValue", () => {
+        render(<TextInput onChange={fn} searchValue="testando"/>);
+
+        const input = screen.getByPlaceholderText(/type your search/i);
+
+        expect(input.value).toBe('testando');
+    });
+
+    it("should call handleChange function on each key pressed", () => {
+        const fn = jest.fn();
+        render(<TextInput handleChange={fn}/>);
+
+        const input = screen.getByPlaceholderText(/type your search/i);
+
+        const value = 'o valor'
+
+        userEvent.type(input, value);
+
+        expect(input.value).toBe(value);
+        expect(fn).toHaveBeenCalledTimes(value.length);
+    });
+
+    it("should match snapshot", () => {
+        const fn = jest.fn();
+        const { container } = render(<TextInput onChange={fn} searchValue={"testando"}/>)
+
+        expect(container).toMatchSnapshot();
+    })
+})
